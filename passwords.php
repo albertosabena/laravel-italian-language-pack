a4dfce2993480d76c97384e5a038c083b55d62d1
Alberto Sabena <alberto.sabena@gmail.com>
2017-03-20 15:32:39 +0100

    compatibility with Laravel 5.4

--- passwords.php
+++ passwords.php
@@ -2,21 +2,21 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
 
-	"password" => "Le password devono contenere almeno sei caratteri e coincidere con la conferma.",
-	"user" => "Impossibile trovare un utente con l'indirizzo email inserito.",
-	"token" => "Il token di reset password non è valido.",
-	"sent" => "Ti è stato inviato il link per il reset della password!",
-	"reset" => "La tua password è stata resettata!",
+    'password' => 'Le password devono corrispondere e contenere almeno 6 caratteri.',
+    'reset' => 'La tua password è stata resettata!',
+    'sent' => 'Ti abbiamo inviato il link di reset password via email!',
+    'token' => 'Il token di reset password non è valido.',
+    'user' => "Non è possibile trovare un utente con l'indirizzo email inserito.",
 
 ];
