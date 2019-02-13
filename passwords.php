c924a8d9b897dae69dd8d7cd66cb7825fc21b76e
unknown <info@archistico.com>
2019-02-13 18:18:07 +0100

    compatibility with Laravel 5.7

--- passwords.php
+++ passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Le password devono corrispondere e contenere almeno 6 caratteri.',
-    'reset' => 'La tua password è stata resettata!',
-    'sent' => 'Ti abbiamo inviato il link di reset password via email!',
-    'token' => 'Il token di reset password non è valido.',
-    'user' => "Non è possibile trovare un utente con l'indirizzo email inserito.",
+    'password' => "Le password devono corrispondere e contenere almeno 6 caratteri.",
+    'reset'    => "La tua password è stata resettata!",
+    'sent'     => "Ti abbiamo inviato il link per il reset della password via email!",
+    'token'    => "Il token di reset della password non è valido.",
+    'user'     => "Non è possibile trovare un utente con l'indirizzo email inserito.",
 
 ];
